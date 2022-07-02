c1c6a04b1a5a6ce54984ab8a31abd2928383aa7f
Amar Cerim <amar.cerim@zeforge.ba>
2022-07-02 20:42:53 +0200

    changed footer info

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="container pt-5">
     <div class="row">
-        <p class="mb-0">Školska 12, 72 000 Zenica, Bosnia and Herzegovina</p>
+        <p class="mb-0">Školska 10, 72 000 Zenica, Bosnia and Herzegovina</p>
     </div>
     <div class="row">
         <p class="mb-0">+387 63 017 661</p>
